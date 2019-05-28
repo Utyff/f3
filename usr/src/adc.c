fd304cce3ed085012823ce409dd716c76641c4c6
Utyf <utyff@mail.ru>
2019-05-28 13:36:10 +0300

    change adc

--- usr/src/adc.c
+++ usr/src/adc.c
@@ -17,24 +17,28 @@ uint32_t dataPoints34[MAX_SAMPLES];
 
 void ADC_set() {
     // enable the ADC voltage regulator
-    ADC1->CR &= ~(1u << 29u); // ADC_CR_ADVREGEN_0
-    ADC2->CR &= ~(1u << 29u);
+    ADC1->CR &= ~ADC_CR_ADVREGEN_1;
+    ADC2->CR &= ~ADC_CR_ADVREGEN_1;
 //    ADC3->CR &= ~(1u << 29u);
 //    ADC4->CR &= ~(1u << 29u);
 
-    ADC1->CR |= (1u << 28u);  // ADC_CR_ADVREGEN_1
-    ADC2->CR |= (1u << 28u);
+    ADC1->CR |= ADC_CR_ADVREGEN_0;
+    ADC2->CR |= ADC_CR_ADVREGEN_0;
 //    ADC3->CR |= (1u << 28u);
 //    ADC4->CR |= (1u << 28u);
 
-    RCC->CFGR2 |= (0b10000u << 4u); // Prescaler RCC_CFGR2_ADCPRE12_DIV1
-//    RCC->CFGR2 |= (0b10000u << 9u); // Prescaler RCC_CFGR2_ADCPRE34_DIV1
+    RCC->CFGR2 |= RCC_CFGR2_ADCPRE12_DIV1; // Prescaler ADC12 - PLL clock divided by 1
+//    RCC->CFGR2 |= RCC_CFGR2_ADCPRE34_DIV1; // Prescaler ADC34 - PLL clock divided by 1
     RCC->AHBENR |= RCC_AHBENR_ADC12EN; // turn on ADC12 clock
 //    RCC->AHBENR |= RCC_AHBENR_ADC34EN; // turn on ADC34 clock
 
     // Set ADC clock
-    ADC12_COMMON->CCR |= (0b01u << 16u); // ADC_CCR_CKMODE_0
-//    ADC34_COMMON->CCR |= (0b01u << 16u); // ADC_CCR_CKMODE_0
+    // 00: (Asynchronous clock mode) PLL used
+    // 01: HCLK/1 (Synchronous clock mode) AHB used
+    // 10: HCLK/2
+    // 11: HCLK/4
+    ADC12_COMMON->CCR |= (0b01u << ADC_CCR_CKMODE_Pos);
+//    ADC34_COMMON->CCR |= (0b01u << ADC_CCR_CKMODE_Pos);
 
     // disable the ADC
     ADC1->CR &= ~ADC_CR_ADEN;
@@ -68,26 +72,35 @@ void ADC_set() {
 //    ADC3->CR |= ADC_CR_ADEN;
 //    ADC4->CR |= ADC_CR_ADEN;
 
-    while (!(ADC1->ISR & (1u << 0u))); // ADC_ISR_ADRDY
-    while (!(ADC2->ISR & (1u << 0u)));
+    // wait till ADC start
+    while (!(ADC1->ISR & ADC_ISR_ADRDY));
+    while (!(ADC2->ISR & ADC_ISR_ADRDY));
 //    while (!(ADC3->ISR & (1u << 0u)));
 //    while (!(ADC4->ISR & (1u << 0u)));
 
     // Select ADC Channels
-    ADC1->SQR1 = (1u << 6u);
-    ADC2->SQR1 = (1u << 6u);
+    ADC1->SQR1 = (1u << ADC_SQR1_SQ1_Pos); // 1-st channel for ADC1
+    ADC2->SQR1 = (1u << ADC_SQR1_SQ1_Pos); // 1-st channel for ADC2
 //    ADC3->SQR1 = (1u << 6u);
 //    ADC4->SQR1 = (3u << 6u);
 
     // Set sampling time for regular group 1
-    ADC1->SMPR1 |= (0b000u << 3u); // 0b000 -> 1.5 clock cycles, shortest available sampling time
-    ADC2->SMPR1 |= (0b000u << 3u); // ADC_SMPR1_SMP0
+    // 000: 1.5 ADC clock cycles
+    // 001: 2.5 ADC clock cycles
+    // 010: 4.5 ADC clock cycles
+    // 011: 7.5 ADC clock cycles
+    // 100: 19.5 ADC clock cycles
+    // 101: 61.5 ADC clock cycles
+    // 110: 181.5 ADC clock cycles
+    // 111: 601.5 ADC clock cycles
+    ADC1->SMPR1 |= (0b000u << ADC_SMPR1_SMP1_Pos);
+    ADC2->SMPR1 |= (0b000u << ADC_SMPR1_SMP1_Pos);
 //    ADC3->SMPR1 |= (0b000u << 3u);
 //    ADC4->SMPR1 |= (0b000u << 3u);
 
-    // Regular sequence settings
-    ADC1->SQR1 |= (0b0000u << 0u); // One conversion in the regular sequence
-    ADC2->SQR1 |= (0b0000u << 0u);
+    // Regular channel sequence length
+    ADC1->SQR1 |= (0b0000u << ADC_SQR1_L_Pos); // One conversion in the regular sequence
+    ADC2->SQR1 |= (0b0000u << ADC_SQR1_L_Pos);
 //    ADC3->SQR1 |= (0b0000u << 0u);
 //    ADC4->SQR1 |= (0b0000u << 0u);
 
@@ -102,24 +115,23 @@ void ADC_set() {
 //    ADC4->CFGR |= 0b00u << ADC_CFGR_RES_Pos;
 
     // Enable continuous conversion mode. Only on the masters
-    ADC1->CFGR |= (1u << 13u); // Master ADC1 + ADC2
+    ADC1->CFGR |= ADC_CFGR_CONT; // Master ADC1 + ADC2
 //    ADC3->CFGR |= (1u << 13u); // Master ADC3 + ADC4
 
     // dual mode - Regular simultaneous mode only
-    ADC12_COMMON->CCR |= (0b00110u << 0u);
+    ADC12_COMMON->CCR |= (0b00110u << ADC_CCR_DUAL_Pos);
 //    ADC34_COMMON->CCR |= (0b00110u << 0u);
 
     // DMA mode.  0 -> One Shot; 1 -> Circular
-    ADC12_COMMON->CCR |= (0u << 13u);
+    ADC12_COMMON->CCR &= ~ADC_CCR_DMACFG;
 //    ADC34_COMMON->CCR |= (0u << 13u);
 
-    // DMA mode for 12-bit resolution
-    ADC12_COMMON->CCR |= (0b10u << 14u);
+    // DMA mode b10 - for 12-bit resolution
+    ADC12_COMMON->CCR |= (0b10u << ADC_CCR_MDMA_Pos);
 //    ADC34_COMMON->CCR |= (0b10u << 14u);
 }
 
 void DMA_init(void) {
-
     // Enable clocks
     RCC->AHBENR |= RCC_AHBENR_DMA1EN; // (1u << 0u); // DMA1
 //    RCC->AHBENR |= (1u << 1u); // DMA2
@@ -129,21 +141,21 @@ void DMA_init(void) {
 //    DMA2_Channel5->CCR |= (1u << 1u);
 
     // Memory increment mode
-    DMA1_Channel1->CCR |= (1u << 7u);
+    DMA1_Channel1->CCR |= DMA_CCR_MINC;
 //    DMA2_Channel5->CCR |= (1u << 7u);
 
     // Peripheral size
     // 00: 8-bits
     // 01: 16-bits
     // 10: 32-bits
-    DMA1_Channel1->CCR |= (0b10u << 8u);
+    DMA1_Channel1->CCR |= (0b10u << DMA_CCR_PSIZE_Pos);
 //    DMA2_Channel5->CCR |= (0b11u << 8u);
 
     // Memory size
     // 00: 8-bits
     // 01: 16-bits
     // 10: 32-bits
-    DMA1_Channel1->CCR |= (0b10u << 10u);
+    DMA1_Channel1->CCR |= (0b10u << DMA_CCR_MSIZE_Pos);
 //    DMA2_Channel5->CCR |= (0b11u << 10u);
 
     // Number of data to transfer
@@ -158,9 +170,11 @@ void DMA_init(void) {
     DMA1_Channel1->CMAR = (uint32_t)(&dataPoints12);
 //    DMA2_Channel5->CMAR = (uint32_t)(&dataPoints34);
 
-    // Reset flags
+    // Reset flags  DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1
     DMA1->IFCR |= 0xFFu;
 //    DMA2->IFCR |= 0xFFu;
+
+    NVIC_EnableIRQ(DMA1_Channel1_IRQn); // enable DMA1 CH1 interrupt
 }
 
 void ADC_takeSamples(void) {
@@ -191,24 +205,23 @@ void ADC_takeSamples(void) {
 //    DMA2->IFCR |= (0b1111111111111111111111111111111u << 0u);
 
     // disable DMA channel
-    DMA1_Channel1->CCR &= ~(1u << 0u);
-//    DMA2_Channel5->CCR &= ~(1u << 0u);
+    DMA1_Channel1->CCR &= ~DMA_CCR_EN;
+//    DMA2_Channel5->CCR &= ~DMA_CCR_EN;
 
     // ADC stop conversion
-    ADC1->CR |= (1u << 4u);
+    ADC1->CR |= ADC_CR_ADSTP;
 //    ADC3->CR |= (1u << 4u);
 
-    while ((ADC1->CR & (1u << 2u)));
+    // wait till ADC stop work
+    while ((ADC1->CR & ADC_CR_ADSTART));
 //    || (ADC3->CR & (1u << 2u)));
 
-    ADC12_COMMON->CCR &= ~(0b10u << 14u);
-//    ADC34_COMMON->CCR &= ~(0b10u << 14u);
-
-    ADC12_COMMON->CCR |= (0b10u << 14u);
+    // DMA mode b10 - for 12-bit resolution
+    ADC12_COMMON->CCR |= (0b10u << ADC_CCR_MDMA_Pos);
 //    ADC34_COMMON->CCR |= (0b10u << 14u);
 
     // ADC start conversion
-    ADC1->CR |= (1u << 2u);
+    ADC1->CR |= ADC_CR_ADSTART;
 //    ADC3->CR |= (1u << 2u);
 }
 
@@ -222,10 +235,10 @@ void ADC_init() {
 //    Serial.begin(57600);
 
     // Enable clocks
-    RCC->AHBENR |= (1u << 17u); // GPIOA
-    RCC->AHBENR |= (1u << 18u); // GPIOB
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // GPIOA
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // GPIOB
 
-    // Set ADC pins to analog input
+    // Set mode b11 (analog input) for ADC pins
     GPIOA->MODER |= (0b11u << 0u); // PA0 for ADC1
 //    GPIOA->MODER |= (0b11u << 8u); // PA4 for ADC2
 //    GPIOB->MODER |= (0b11u << 2u); // PB1 for ADC3
@@ -236,7 +249,7 @@ void ADC_init() {
     ADC_set();
 
     // Start conversion
-    ADC1->CR |= (1u << 2u);
+    ADC1->CR |= ADC_CR_ADSTART;
 //    ADC3->CR |= (1u << 2u);
 }
 
