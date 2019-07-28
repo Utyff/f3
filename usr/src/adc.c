4142de81755d55c693dc84f84ce105178c6ea99e
Utyf <utyff@mail.ru>
2019-07-28 18:33:49 +0300

    adc merge 2

--- usr/src/adc.c
+++ usr/src/adc.c
@@ -63,44 +63,55 @@ void DMA_Init();
 void ADC_Init() {
     DMA_Init();
 
-    // Enable clocks GPIOA
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+        // Enable clocks GPIOA
+        RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-    // Set mode b11 (analog input) for ADC pins
-    GPIOA->MODER |= (0b11u << 0u);  // PA0 for ADC1 ch1
-    GPIOA->MODER |= (0b11u << 10u); // PA5 for ADC2 ch2
+        // Set mode b11 (analog input) for ADC pins
+        GPIOA->MODER |= (0b11u << 0u);  // PA0 for ADC1 ch1
+        GPIOA->MODER |= (0b11u << 10u); // PA5 for ADC2 ch2
 
-    // enable the ADC voltage regulator
-    ADC1->CR &= ~ADC_CR_ADVREGEN_1;
-    ADC2->CR &= ~ADC_CR_ADVREGEN_1;
+        // enable the ADC voltage regulator
+        ADC1->CR &= ~ADC_CR_ADVREGEN_1;
+        ADC2->CR &= ~ADC_CR_ADVREGEN_1;
 
-    ADC1->CR |= ADC_CR_ADVREGEN_0;
-    ADC2->CR |= ADC_CR_ADVREGEN_0;
+        ADC1->CR |= ADC_CR_ADVREGEN_0;
+        ADC2->CR |= ADC_CR_ADVREGEN_0;
+        delay_us(10); // voltage regulator startup time
 
-    RCC->CFGR2 |= RCC_CFGR2_ADCPRE12_DIV1; // Prescaler ADC12 - PLL clock divided by 1
-    RCC->AHBENR |= RCC_AHBENR_ADC12EN; // turn on ADC12 clock
+        RCC->AHBENR |= RCC_AHBENR_ADC12EN; // turn on ADC12 clock
 
-    // Set ADC clock
-    // 00: (Asynchronous clock mode) PLL used
-    // 01: HCLK/1 (Synchronous clock mode) AHB used
-    // 10: HCLK/2
-    // 11: HCLK/4
-    ADC12_COMMON->CCR &= ~ADC_CCR_CKMODE_Msk;
-    ADC12_COMMON->CCR |= ADC_CCR_CKMODE_0;
+        // Set Prescaler ADC for Asynchronous clock mode
+        RCC->CFGR2 = MODIFY_REG(RCC->CFGR2, RCC_CFGR2_ADCPRE12_Msk, rccAdcDivider << RCC_CFGR2_ADCPRE12_Pos);
 
-    // disable the ADC
-    ADC1->CR &= ~ADC_CR_ADEN;
-    ADC2->CR &= ~ADC_CR_ADEN;
+        // Set ADC clock
+        // 00: (Asynchronous clock mode) PLL
+        // 01: HCLK/1 (Synchronous clock mode) AHB
+        // 10: HCLK/2
+        // 11: HCLK/4
+        ADC12_COMMON->CCR &= ~ADC_CCR_CKMODE_Msk;
+        ADC12_COMMON->CCR |= ADC_CCR_CKMODE_0;
 
-    // start ADC calibration cycle
-    ADC1->CR |= ADC_CR_ADCAL;
-    // wait for calibration to complete
-    while (ADC1->CR & ADC_CR_ADCAL);
+        // start ADC calibration cycle
+        ADC1->CR |= ADC_CR_ADCAL;
+        // wait for calibration to complete
+        while (ADC1->CR & ADC_CR_ADCAL);
 
-    // start ADC calibration cycle
-    ADC2->CR |= ADC_CR_ADCAL;
-    // wait for calibration to complete
-    while (ADC2->CR & ADC_CR_ADCAL);
+        // start ADC calibration cycle
+        ADC2->CR |= ADC_CR_ADCAL;
+        // wait for calibration to complete
+        while (ADC2->CR & ADC_CR_ADCAL);
+
+    // Set Prescaler ADC for Asynchronous clock mode
+    RCC->CFGR2 = MODIFY_REG(RCC->CFGR2, RCC_CFGR2_ADCPRE12_Msk, rccAdcDivider << RCC_CFGR2_ADCPRE12_Pos);
+
+    // Delay for interleaved mode. Set only when ADEN=0
+    ADC12_COMMON->CCR |= (adcDelay << ADC_CCR_DELAY_Pos);
+
+    // dual mode. Set only when ADEN=0
+    // 00000: Independent mode
+    // 00110: Regular simultaneous mode only
+    // 00111: Interleaved mode only
+    ADC12_COMMON->CCR |= (0b00110u << ADC_CCR_DUAL_Pos);
 
     // enable the ADC
     ADC1->CR |= ADC_CR_ADEN;
@@ -114,40 +125,23 @@ void ADC_Init() {
     ADC1->SQR1 = (1u << ADC_SQR1_SQ1_Pos); // 1-st channel for ADC1
     ADC2->SQR1 = (2u << ADC_SQR1_SQ1_Pos); // 2-d  channel for ADC2
 
-    // Set sampling time for regular group 1
-    // 000: 1.5 ADC clock cycles
-    // 001: 2.5 ADC clock cycles
-    // 010: 4.5 ADC clock cycles
-    // 011: 7.5 ADC clock cycles
-    // 100: 19.5 ADC clock cycles
-    // 101: 61.5 ADC clock cycles
-    // 110: 181.5 ADC clock cycles
-    // 111: 601.5 ADC clock cycles
+    // Set sampling time for channels
     ADC1->SMPR1 |= (sampleTime << ADC_SMPR1_SMP1_Pos);
     ADC2->SMPR1 |= (sampleTime << ADC_SMPR1_SMP2_Pos);
 
-    // Regular channel sequence length
-    ADC1->SQR1 |= (0b0000u << ADC_SQR1_L_Pos); // One conversion in the regular sequence
+    // Regular channel sequence length - 1
+    ADC1->SQR1 |= (0b0000u << ADC_SQR1_L_Pos);
     ADC2->SQR1 |= (0b0000u << ADC_SQR1_L_Pos);
 
     // set data resolution
-    // 00: 12-bit
-    // 01: 10-bit
-    // 10: 8-bit
-    // 11: 6-bit
-    ADC1->CFGR |= 0b10u << ADC_CFGR_RES_Pos;
-    ADC2->CFGR |= 0b10u << ADC_CFGR_RES_Pos;
+    ADC1->CFGR |= adcResolution << ADC_CFGR_RES_Pos;
+    ADC2->CFGR |= adcResolution << ADC_CFGR_RES_Pos;
 
     // Enable continuous conversion mode. Only on the masters
     ADC1->CFGR |= ADC_CFGR_CONT; // Master ADC1 + ADC2
 
-    // dual mode. Set only when ADEN=0
-    // 00000: Independent mode
-    // 00110: Regular simultaneous mode only
-    // 00111: Interleaved mode only
-    ADC12_COMMON->CCR |= (0b00110u << ADC_CCR_DUAL_Pos);
-
     // DMA mode.  0 -> One Shot; 1 -> Circular
+    // 0 - stop when DMA_CCR_TCIE
     ADC12_COMMON->CCR &= ~ADC_CCR_DMACFG;
 
     // COMMON DMA mode b11 - for 8-bit resolution
@@ -185,8 +179,8 @@ void DMA_Init() {
     // Memory address register
     DMA1_Channel1->CMAR = (uint32_t) (&samplesBuffer);
 
-    // Reset flags  DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1
-    DMA1->IFCR |= 0xFFu;
+    // Reset interrupt flags
+    DMA1->IFCR |= DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1;
 
     NVIC_EnableIRQ(DMA1_Channel1_IRQn); // enable DMA1 CH1 interrupt
 }
@@ -215,11 +209,9 @@ void ADC_StartSamples(void) {
 
     // ADC stop conversion
     ADC1->CR |= ADC_CR_ADSTP;
-
     // wait till ADC stop work
     while ((ADC1->CR & ADC_CR_ADSTART));
 }
-
 
 uint16_t dmaG = 0;
 uint16_t dmaT = 0;
@@ -230,6 +222,7 @@ uint16_t dma = 0;
 void DMA1_Channel1_IRQHandler() {
     dma++;
     if (DMA1->ISR & DMA_ISR_TCIF1) { // transfer complete
+        samplesReady = 1;
         DMA1->IFCR |= DMA_IFCR_CTCIF1;
         dmaT++;
     }
